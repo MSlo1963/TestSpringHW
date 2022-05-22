083af4408cb748c749701e7fdda104162ca88d76
Marc Slowack <MSlo1963@outlook.com>
2022-05-22 11:45:28 +0200

    fix overfoldering

---
+++ src/main/java/com/msl/test/TestSpringHw1Application.java
@@ -0,0 +1,13 @@
+package com.msl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringHw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringHw1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/msl/test/TestSpringHw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringHw1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
